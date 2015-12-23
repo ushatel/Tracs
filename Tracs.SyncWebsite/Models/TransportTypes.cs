6ab257ece095079e1b98ccb8e0a86bb9f7e603e9
Oleg V Sysprog <ok@kazka.me>
2015-12-23 02:07:38 +0200

    TransportTypes Added

---
+++ Tracs.SyncWebsite/Models/TransportTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tracs.SyncWebsite.Models
+{
+    public class TransportTypes
+    {
+        public int TransportTypesID { get; set; }
+
+        /// <summary>
+        /// Name is title of TransportType 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Types of vehicles by motoric function - water, air, wheel, rail 
+        /// </summary>
+        public string MoveType { get; set; }
+    }
+}
